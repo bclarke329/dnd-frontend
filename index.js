b5d053b3fb34f38d3024605d2b42579932877879
bclarke329 <bmendoza329@hotmail.com>
2021-04-22 23:55:16 -0500

    More dropdowns added with fetched data

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const baseUrl = "https://www.dnd5eapi.co/api/"
 
 document.addEventListener('DOMContentLoaded', getSpecies)
 document.addEventListener('DOMContentLoaded', getCharacterClass)
+document.addEventListener('DOMContentLoaded', fetchAlignments)
+document.addEventListener('DOMContentLoaded', fetchWeapons)
+
 
 //fetching race names
 function getSpecies() {
     fetch('https://www.dnd5eapi.co/api/races')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        // console.log(data)
 
         addSpeciesToDrop(data)
     })
@@ -60,5 +63,61 @@ function addCharacterClassesToDrop(data){
         options.text = classesArray[i].name
         classDropdown.add(options)
     }
+}
 
+function fetchAlignments() {
+    fetch('https://www.dnd5eapi.co/api/alignments')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data)
+
+    addAlignmentsToDrop(data)
+  })
+}
+
+function addAlignmentsToDrop(data) {
+    let alignmentArray = data["results"]
+    let alignmentDropDown = document.getElementById('alignment-dropdown')
+    alignmentDropDown.length = 0
+
+    let thirdDefaultOption = document.createElement('option')
+    thirdDefaultOption.text = "Choose An Alignment"
+    alignmentDropDown.add(thirdDefaultOption)
+    alignmentDropDown.selectedIndex = 0
+
+    let options;
+    for (let i = 0; i < alignmentArray.length; i++) {
+        options = document.createElement('option')
+        options.text = alignmentArray[i].name
+        alignmentDropDown.add(options)
+    }
+}
+
+
+function fetchWeapons() {
+    fetch('https://www.dnd5eapi.co/api/equipment-categories/weapon')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+    addEquipmentToDrop(data)
+  })
+}
+
+function addEquipmentToDrop(data) {
+    let equipmentObject= data["equipment"]
+    debugger
+    let equipmentDropdown = document.getElementById('equipment-dropdown')
+    equipmentDropdown.length = 0
+    let fourthDefaultOption = document.createElement('option')
+    fourthDefaultOption.text = "Choose A Weapon"
+    equipmentDropdown.add(fourthDefaultOption)
+    equipmentDropdown.selectedIndex = 0
+
+    let options;
+    for (let weapon in equipmentObject) {
+        options = document.createElement('option')
+        options.text = `${weapon.name}`
+        equipmentDropdown.add(options)
+    }
 }
